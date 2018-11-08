804b67642b2af3116a6375a0ded34fe59bdf1235
Kaai Suzuki <suzydp@me.com>
2018-11-08 01:31:42 -0800

    7 - Make details of Artworks component

--- src/components/ArtWorks/ArtWorks.jsx
+++ src/components/ArtWorks/ArtWorks.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import lipstick from './../../assets/img/lipstick.png';
 import './ArtWorks.scss';
 
 export default class ArtWorks extends Component {
@@ -6,7 +7,14 @@ export default class ArtWorks extends Component {
         return (
             //contents which you want to appear on browser
             <div className="ArtWorks">
-                <p>This is an ArtWorks component.</p>
+                <div className="ArtWorks-img">
+                    <img src={lipstick} alt="lipstick" />
+                    <button className="ArtWorks-btn">See details</button>
+                </div>
+                <div className="Artworks-details">
+                    <h3>Title</h3>
+                    <p>Categories</p>
+                </div>
             </div>
         )
     }
