39099c64a75c86d842f61bb4025c326fe61c3701
Kaai Suzuki <suzydp@me.com>
2018-11-26 20:31:53 -0800

    12 - Add another package - RE-commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import Media from 'react-media';
 import './Header.scss';
 
 export default class Header extends Component {
-    render() {
-        return (
-            //contents which you want to appear on browser
-            <header className="Header">
-                <div className="Header-inner">
-                    <h1 className="Header-title">Artworks by SZDP</h1>
-                    <ul className="Header-menu">
-                        <li>Top</li>
-                        <li>Works</li>
-                        <li>Bio</li>
-                        <li>Contact</li>
-                        <li><a href="http://suzydp.net">suzydp.net</a></li>
-                    </ul>
-                </div>
-            </header>
-        )
-    }
+	render() {
+		return (
+			<header className="Header">
+				<div className="Header-inner">
+					<h1 className="Header-title">Artworks by SZDP</h1>
+					<Media query="(min-width:540px)">
+						{/* 540px以上なら以下を表示 */}
+						{(matches) => (matches 
+						? 
+						<ul className="Header-menu">
+							<li>Top</li>
+							<li>Works</li>
+							<li>Bio</li>
+							<li>Contact</li>
+							<li><a href="http://suzydp.net">suzydp.net</a></li>
+						</ul>
+						:
+						<img 
+							width={20}
+							alt="Menu Icon"
+							src={require("./../../assets/icons/menuIcons.svg")}
+						/>
+						)}
+					</Media>
+				</div>
+			</header>
+		)
+	}
 }
 
