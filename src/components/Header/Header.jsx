b0d06959a6c76a3350689d41ff2f5161cedd7d56
Kaai Suzuki <suzydp@me.com>
2018-12-01 00:19:42 -0800

    22 - Preparation for toggle navigation on Smartphone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Media from 'react-media';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 export default class Header extends Component {
+
+	// define handleClick to open button in Smartphone view
+	handleClick = () =>{
+		console.log("Clicked!");
+	}
+
 	render() {
 		return (
 			<header className="Header">
@@ -13,44 +19,26 @@ export default class Header extends Component {
 						{/* 540px以上なら以下を表示 (三項演算子を使用して条件分岐している)*/}
 						{/* Header.jsx内のNavlinkのpathと、App.jsのPathが一致すること */}
 						{(matches) => (matches 
-						? 
-						<div className="Header-menu">
-							<NavLink 
-								exact
-								to="/" 
-								className="Header-menu-item"
-								activeClassName="Header-menu-item--active">
-								Top
-							</NavLink>
-							<NavLink 
-								to="/works/" 
-								className="Header-menu-item"
-								activeClassName="Header-menu-item--active">
-								Works
-							</NavLink>
-							<NavLink 
-								to="/biography" 
-								className="Header-menu-item"
-								activeClassName="Header-menu-item--active">
-								Bio
-							</NavLink>
-							<NavLink to="/contact" 
-								className="Header-menu-item"
-								activeClassName="Header-menu-item--active">
-								Contact
-							</NavLink>
-							<a href="http://suzydp.net" 
-								className="Header-menu-item"
-								activeClassName="Header-menu-item--active">
-								suzydp.net
-							</a>
-						</div>
-						:
-						<img 
-							width={30}
-							alt="Menu Icon"
-							src={require("./../../assets/icons/menuIcons.svg")}
-						/>
+							? (
+							<div className="Header-menu">
+								<Nav />
+							</div>
+							) : (
+								// Dropdown NavLink for mobile view
+								<div className="Header-Dropdown">
+									<button onClick={this.handleClick}>
+										<img
+											width={30}
+											alt="Menu Icon"
+											src={require("./../../assets/icons/menuIcons.svg")}
+										/>
+									</button>
+									<div className="Header-dropdown-menu">
+										{/* Place <Nav /> inside of dropdown-menu class */}
+										<Nav />
+									</div>
+								</div>
+							)
 						)}
 					</Media>
 				</div>
@@ -59,3 +47,41 @@ export default class Header extends Component {
 	}
 }
 
+// Make function for appearing Nav with Fragment which defined above.
+// *this Nav is replaced to <Nav /> above. 
+const Nav = () => {
+	return (
+		<Fragment>
+			{/* replace from above to here */}
+			<NavLink
+				exact
+				to="/"
+				className="Header-menu-item"
+				activeClassName="Header-menu-item--active">
+				Top
+			</NavLink>
+			<NavLink
+				to="/works/"
+				className="Header-menu-item"
+				activeClassName="Header-menu-item--active">
+				Works
+			</NavLink>
+			<NavLink
+				to="/biography"
+				className="Header-menu-item"
+				activeClassName="Header-menu-item--active">
+				Bio
+			</NavLink>
+			<NavLink to="/contact"
+				className="Header-menu-item"
+				activeClassName="Header-menu-item--active">
+				Contact
+			</NavLink>
+			<a href="http://suzydp.net"
+				className="Header-menu-item"
+				activeClassName="Header-menu-item--active">
+				suzydp.net
+			</a>
+		</Fragment>
+	)
+}
