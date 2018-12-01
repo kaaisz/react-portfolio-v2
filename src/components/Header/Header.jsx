2866cba2d33145d74b9513679fe80954fd36b057
Kaai Suzuki <suzydp@me.com>
2018-12-01 01:02:28 -0800

    23 - Add styles on Header dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Media from 'react-media';
+import cx from 'classnames';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
@@ -21,11 +22,11 @@ export default class Header extends Component {
 						{(matches) => (matches 
 							? (
 							<div className="Header-menu">
-								<Nav />
+								<Nav smallSize={false}/>
 							</div>
 							) : (
 								// Dropdown NavLink for mobile view
-								<div className="Header-Dropdown">
+								<div className="Header-dropdown">
 									<button onClick={this.handleClick}>
 										<img
 											width={30}
@@ -35,7 +36,7 @@ export default class Header extends Component {
 									</button>
 									<div className="Header-dropdown-menu">
 										{/* Place <Nav /> inside of dropdown-menu class */}
-										<Nav />
+										<Nav smallSize={true}/>
 									</div>
 								</div>
 							)
@@ -49,36 +50,42 @@ export default class Header extends Component {
 
 // Make function for appearing Nav with Fragment which defined above.
 // *this Nav is replaced to <Nav /> above. 
-const Nav = () => {
+// smallSizeを引数として渡し、smallSizeがtrueの時に'Header-nav-item--dropdown'が表示されるようにclassnamesでわけている
+const Nav = ({smallSize}) => {
 	return (
 		<Fragment>
 			{/* replace from above to here */}
 			<NavLink
 				exact
 				to="/"
-				className="Header-menu-item"
+				className=
+				{cx("Header-menu-item", { 'Header-nav-item--dropdown': smallSize})}
 				activeClassName="Header-menu-item--active">
 				Top
 			</NavLink>
 			<NavLink
 				to="/works/"
-				className="Header-menu-item"
+				className=
+				{cx("Header-menu-item", { 'Header-nav-item--dropdown': smallSize})}
 				activeClassName="Header-menu-item--active">
 				Works
 			</NavLink>
 			<NavLink
 				to="/biography"
-				className="Header-menu-item"
+				className=
+				{cx("Header-menu-item", { 'Header-nav-item--dropdown': smallSize})}
 				activeClassName="Header-menu-item--active">
 				Bio
 			</NavLink>
 			<NavLink to="/contact"
-				className="Header-menu-item"
+				className=
+				{cx("Header-menu-item", { 'Header-nav-item--dropdown': smallSize})}
 				activeClassName="Header-menu-item--active">
 				Contact
 			</NavLink>
 			<a href="http://suzydp.net"
-				className="Header-menu-item"
+				className=
+				{cx("Header-menu-item", { 'Header-nav-item--dropdown': smallSize})}
 				activeClassName="Header-menu-item--active">
 				suzydp.net
 			</a>
